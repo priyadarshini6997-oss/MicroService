1bfefa195113d394ededb038246797476e0cbcf5
Priyadarshini C <priyadarshini6997@gmail.com>
2026-02-16 21:38:43 +0530

    Add files via upload

---
+++ MicroServicesSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesSampleApplication.class, args);
+	}
+
+}
